c7bacb735f8b2e40a0f9543197f215fddbd7b83c
Anderson Francelino <anderson.francelino@gmail.com>
2026-02-01 22:15:48 +0000

    personalizando projeto

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './component/header/header';
+import { Main } from "./component/main/main";
+import { Footer } from './component/footer/footer';
+;
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ Header, Main, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
